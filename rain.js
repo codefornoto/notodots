ffeb7308fff19248bccde4218302a56381c9661a
kenshgy <ken.shgy.1386@gmail.com>
2024-04-11 18:41:28 +0900

    update

--- rain.js
+++ rain.js
@@ -1,24 +1,14 @@
 var c = document.getElementById("canvas-club");
 var ctx = c.getContext("2d");
 var drops = [];
-var max = 1;
+var max = 7;
 var clearColor = "rgba(0, 0, 0, .1)";
 const BACKEND_URL =
   "https://script.google.com/macros/s/AKfycbwQBJ7HBB3HwF1YArbfeSINTN229HFzo1AJU13EF1ftT7VtIPGjvDwoZrvh9sKDE_Av0Q/exec";
-let texts = ["text1", "text2", "text3"];
+let texts = ["NOTODOTS powered by Code for Noto"];
 this.textIndex = 0;
 
 const sleep = (time) => new Promise((r) => setTimeout(r, time)); //timeはミリ秒
-
-async function resetAnimation() {
-  drops.length = 0;
-  if (textIndex == texts.length) {
-    this.textIndex = 0;
-  } else {
-    this.textIndex += 1;
-  }
-  setup();
-}
 
 async function getDataFromSpreadSheet() {
   $.ajax({
@@ -28,7 +18,7 @@ async function getDataFromSpreadSheet() {
     // 成功した時の処理
     console.log("called api");
     texts = JSON.parse(result);
-    resetAnimation();
+    console.log(texts);
   });
   // .fail((error) => {
   //   // 失敗した時の処理
@@ -38,6 +28,16 @@ async function getDataFromSpreadSheet() {
   //   // 常にやる処理
   //   // do something
   // });
+}
+
+async function resetAnimation() {
+  drops.length = 0;
+  if (textIndex == texts.length) {
+    this.textIndex = 0;
+  } else {
+    this.textIndex += 1;
+  }
+  setup();
 }
 
 function random(min, max) {
@@ -50,12 +50,11 @@ function O(texts) {
 
 O.prototype = {
   init: function () {
-    getDataFromSpreadSheet();
     this.x = random(300, window.innerWidth - 300);
     this.color = "hsl(" + random(0, 360) + ", 100%, 50%)";
     this.w = 350;
     this.h = 100;
-    this.vy = random(4, 5);
+    this.vy = random(1, 10);
     this.vw = 3;
     this.vh = 1;
     // this.vw = 10;
@@ -68,8 +67,9 @@ O.prototype = {
     this.va = 0.98;
     this.waveColor = "hsla(" + random(0, 360) + ", 100%, 50%, " + this.a + ")";
     this.alpha = 0;
-    this.y = 300; //random(200, window.innerHeight + 100);
-    this.text = texts[textIndex]; // テキストを設定
+    this.y = random(-200, window.innerHeight + 100);
+    console.log(this.y);
+    this.text = texts[Math.floor(Math.random() * texts.length)]; // テキストを設定
     // this.text = this.texts[Math.floor(Math.random() * this.texts.length)];
 
     if (this.alpha > 0.00000001) {
@@ -116,7 +116,7 @@ O.prototype = {
       if (this.alpha < 1) {
         this.alpha += 0.02;
       } else {
-        this.alpha -= 0.02;
+        this.alpha -= 0.0002;
       }
 
       ctx.globalAlpha = 1;
@@ -153,6 +153,7 @@ function resizeCanvas() {
 }
 
 function setup() {
+  getDataFromSpreadSheet();
   for (var i = 0; i < max; i++) {
     var o = new O(texts);
     o.init();
@@ -161,7 +162,6 @@ function setup() {
 }
 
 function animate() {
-  console.log("animate");
   ctx.fillStyle = clearColor;
   ctx.fillRect(0, 0, c.width, c.height);
   for (var i in drops) {
